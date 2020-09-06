acf7ee19fb973e37ed0caa54713579f7a62b7e72
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-06 10:29:18 +0100

    show alert if item is added or passing empty item

--- app.js
+++ app.js
@@ -12,8 +12,26 @@ function addItem(e) {
     e.preventDefault()
     let value =  input.value
     if (value === '') {
-        
+        showAction(addItemsAction, 'Please add grocery item', false)
     } else {
+        showAction(addItemsAction, `${value} add to the list`, true)
+    }
+}
 
+function showAction (element, text, value) {
+    if(value === true) {
+        element.classList.add('success')
+        element.innerText = text
+        input.value = ''
+        setTimeout( () => {
+            element.classList.remove('success')
+        }, 3000)
+    } else {
+        element.classList.add('alert')
+        element.innerText = text
+        input.value = ''
+        setTimeout( () => {
+            element.classList.remove('alert')
+        }, 3000)
     }
 }
